01caf26e02f0d569f55a071d6c36f0d49b4548b5
ajay-rawat-9 <84247810+ajay-rawat-9@users.noreply.github.com>
2022-06-13 03:29:35 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_dev="0504c1c5-88ef-4cc4-976a-85d11d7231a4"
-dev_id="BOLT8022712"
-telegram_chat_id="@ajayrawat9"
-telegram_bot_id="bot2135235405:AAF_KhI_mk0-oIFW-fVik7MWIShc89lVqbU"
+api_dev="Your API ID"
+dev_id="Your Bolt IoT Device Name"
+telegram_chat_id="Your Telegram Chat ID"
+telegram_bot_id="Your Telegram Bot ID"
